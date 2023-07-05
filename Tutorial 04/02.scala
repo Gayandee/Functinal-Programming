5d7e6aa72a4db0065f40e520d61b54be350fc34f
Gayandee <118333384+Gayandee@users.noreply.github.com>
2023-07-05 20:53:27 +0530

    Update 02.scala

--- Tutorial 04/02.scala
+++ Tutorial 04/02.scala
@@ -1,1 +1,14 @@
-
+object PatternMatching{
+    def main(args: Array[String]):
+    Unit={
+        println(check(-1))
+        println(check(2))
+        println(check(3))
+    }
+    
+    def check(num:Int):String=num match{
+        case num if(num<=0)=>"Negative/Zero is input"
+        case num if(num%2==0)=>"Even number is given"
+        case _=>"Odd number is given"
+  }
+}
